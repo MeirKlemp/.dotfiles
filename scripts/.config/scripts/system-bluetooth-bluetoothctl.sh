0b63f1204d3a7448805e519367f8f77b0422ba27
Meir Klemfner <meirk251@outlook.com>
2021-12-19 21:43:45 +0200

    polybar: add bluetooth module

---
+++ scripts/.config/scripts/system-bluetooth-bluetoothctl.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+bluetooth_print() {
+    bluetoothctl | while read -r; do
+        if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
+	    if bluetoothctl show | grep -q "Powered: no"; then
+                echo ' off'
+		continue
+	    fi
+            printf ''
+
+            devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+            counter=0
+
+            for device in $devices_paired; do
+                device_info=$(bluetoothctl info "$device")
+
+                if echo "$device_info" | grep -q "Connected: yes"; then
+                    device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+
+                    if [ $counter -gt 0 ]; then
+                        printf ", %s" "$device_alias"
+                    else
+                        printf " %s" "$device_alias"
+                    fi
+
+                    counter=$((counter + 1))
+                fi
+            done
+
+	    if [ $counter = 0 ]; then
+		    printf ' none'
+	    fi
+
+            printf '\n'
+        fi
+    done
+}
+
+bluetooth_toggle() {
+    if bluetoothctl show | grep -q "Powered: yes"; then
+        devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+        echo "$devices_paired" | while read -r line; do
+            bluetoothctl disconnect "$line" >> /dev/null
+        done
+
+        bluetoothctl power off >> /dev/null
+    else
+        bluetoothctl power on >> /dev/null
+        sleep 1
+
+        devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+        echo "$devices_paired" | while read -r line; do
+            bluetoothctl connect "$line" >> /dev/null
+        done
+    fi
+}
+
+case "$1" in
+    --toggle)
+        bluetooth_toggle
+        ;;
+    *)
+        bluetooth_print
+        ;;
+esac
